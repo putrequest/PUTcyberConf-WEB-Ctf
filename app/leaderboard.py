88dd7c7f7623916190600d922efbfaba552c10b3
unsaltedbutter0 <79810314+unsaltedbutter0@users.noreply.github.com>
2023-05-18 21:15:01 +0200

    leaderboard core functionality

---
+++ app/leaderboard.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+def update_leaderboard():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    leaderboard_data = c.execute('SELECT username, points FROM users ORDER BY points DESC').fetchall()
+    conn.close()
+
+    return leaderboard_data
+
+@app.route('/')
+def leaderboard():
+    leaderboard_data = update_leaderboard()
+
+    return render_template('leaderboard.html', leaderboard_data=leaderboard_data)
+
+@app.route('/update_board')
+def update():
+    leaderboard_data = update_leaderboard()
+
+    return render_template('update_board.html', leaderboard_data=leaderboard_data)
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
